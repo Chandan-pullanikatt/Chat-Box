5053285a8964b5b0da683c2a31d46a16c118547d
Chandan-pullanikatt <119943883+Chandan-pullanikatt@users.noreply.github.com>
2024-04-24 22:02:29 +0530

    Add files via upload

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,18 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '4ae12edf-d1fc-4f47-89e6-35b7098ded82',
+        props.user.username,
+        props.user.secret
+    );
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    );
+};
+
+export default ChatsPage;
